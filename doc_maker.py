bd0d79e5d1d8e6bc904a11723b935d548a76be83
oppconnector <jakemgrim@gmail.com>
2025-01-30 05:16:27 -0500

    first commit

---
+++ doc_maker.py
@@ -0,0 +1,18 @@
+#1: open all files in project folder
+
+#2: covert files to raw text
+#3: combine all text into one long string
+
+#4: chunk text
+#5: ai embed/vectorize text chunks
+#6: save to db (pandas for temporary saving)
+
+#7: open template html document
+#8: pull prompts out of html document
+
+#9: awnser each prompt with RAG
+#10: convert RAG answer to HTML
+#11: replace prompt with HTML RAG awnser
+
+#12: convert HTML as pdf file
+#13: save pdf file with template name and date time stamp 
